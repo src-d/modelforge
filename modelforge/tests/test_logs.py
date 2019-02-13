bdcb6af66268c7ec747c598b0e5ac5e1ce85ab50
Vadim Markovtsev <vadim@sourced.tech>
2019-02-13 14:11:23 +0100

    Add slogging.add_logging_args()
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- modelforge/tests/test_logs.py
+++ modelforge/tests/test_logs.py
@@ -1,8 +1,17 @@
+import argparse
+from distutils.util import strtobool
 import logging
+import os
+import subprocess
+import sys
+from typing import Tuple, Union
 import unittest
 
 from modelforge import slogging
 from modelforge.tests.capture import captured_output
+
+
+ARGPARSE_TEST = "MODELFORGE_SLOGGING_TEST"
 
 
 class LogTests(unittest.TestCase):
@@ -18,6 +27,58 @@ class LogTests(unittest.TestCase):
         self.assertIn("success", err.getvalue())
         self.assertIn("1;36", err.getvalue())
 
+    def test_argparse(self):
+        level, structured, config_path = self.launch_test_slogging_main("", False, "")
+        self.assertEqual(level, "INFO")
+        self.assertFalse(structured)
+        self.assertEqual(config_path, "None")
+        level, structured, config_path = self.launch_test_slogging_main("DEBUG", True, "/path")
+        self.assertEqual(level, "DEBUG")
+        self.assertTrue(structured)
+        self.assertEqual(config_path, "/path")
+
+    def test_argparse_no_patch(self):
+        parser = argparse.ArgumentParser()
+        method = parser.parse_args
+        slogging.add_logging_args(parser, False)
+        self.assertEqual(method, parser.parse_args)
+
+        parser = argparse.ArgumentParser()
+        method = parser.parse_args
+        slogging.add_logging_args(parser)
+        self.assertNotEqual(method, parser.parse_args)
+
+    def launch_test_slogging_main(self, level: str, structured: bool, config_path: str
+                                  ) -> Tuple[str, bool, str]:
+        args = [sys.executable, __file__]
+        if config_path:
+            args.extend(["--log-config", config_path])
+        if structured:
+            args.append("--log-structured")
+        if level:
+            args.extend(["--log-level", level])
+        patched_env = os.environ.copy()
+        patched_env[ARGPARSE_TEST] = "1"
+        result = subprocess.check_output(args, env=patched_env).decode().splitlines()
+        result[1] = strtobool(result[1])
+        return tuple(result)
+
+
+def run_slogging_main():
+    parser = argparse.ArgumentParser()
+    slogging.add_logging_args(parser)
+
+    def my_setup(level: Union[str, int], structured: bool, config_path: str):
+        print(level)
+        print(structured)
+        print(config_path)
+
+    slogging.setup = my_setup
+    parser.parse_args()
+
 
 if __name__ == "__main__":
-    unittest.main()
+    if os.getenv(ARGPARSE_TEST, False):
+        run_slogging_main()
+    else:
+        unittest.main()
